0efbe1ccf9138af60f7abed61497f26451fbf683
Asaku1 <ayoghagborbesong@gmail.com>
2026-04-30 03:10:27 -0400

    Restructure repo and add backend with proper .gitignore

---
+++ Lentis.Api/Lentis.Api/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Lentis.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lentis.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Lead> Leads => Set<Lead>();
+    }
+}
---
+++ Lentis.Api/Lentis.Api/Models/Lead.cs
@@ -0,0 +1,15 @@
+﻿namespace Lentis.Api.Models
+{
+    public class Lead
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public string Email { get; set; } = "";
+
+        public string Message { get; set; } = "";
+
+        public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+    }
+}
